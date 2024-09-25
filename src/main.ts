4ee838eb1945abd7703f6e99d1d002e27817677c
Andrew Connor <andrewconnor2@gmail.com>
2024-09-25 22:16:59 +0100

    Configure ESLint and Prettier, remove boilerplate code, update formatting rules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
+import {
+    RouteReuseStrategy,
+    provideRouter,
+    withPreloading,
+    PreloadAllModules
+} from '@angular/router';
+import {
+    IonicRouteStrategy,
+    provideIonicAngular
+} from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        provideIonicAngular(),
+        provideRouter(routes, withPreloading(PreloadAllModules))
+    ]
 });
