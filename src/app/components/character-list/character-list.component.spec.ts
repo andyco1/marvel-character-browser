cba4a1861e898d4dcab8397e9b2c3e9c8648d7c5
Andrew Connor <andrewconnor2@gmail.com>
2024-10-05 20:15:00 +0100

    Character List: Implementation of grid, styling and unit tests

--- src/app/components/character-list/character-list.component.spec.ts
+++ src/app/components/character-list/character-list.component.spec.ts
@@ -1,23 +1,134 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule, LoadingController } from '@ionic/angular';
+import { of, throwError } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { mockEmptyMarvelApiResponse, mockMarvelApiResponse } from 'src/app/shared/testing/mock-data';
 import { CharacterListComponent } from './character-list.component';
 
 describe('CharacterListComponent', () => {
     let component: CharacterListComponent;
     let fixture: ComponentFixture<CharacterListComponent>;
+    let apiServiceSpy: jasmine.SpyObj<ApiService>;
+    let notificationServiceSpy: jasmine.SpyObj<NotificationService>;
+    let loadingControllerSpy: jasmine.SpyObj<LoadingController>;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [CharacterListComponent],
-            imports: [IonicModule.forRoot()]
+    beforeEach(async () => {
+        apiServiceSpy = jasmine.createSpyObj('ApiService', ['getCharacters', 'getCharacterById']);
+        notificationServiceSpy = jasmine.createSpyObj('NotificationService', ['showErrorToast']);
+        loadingControllerSpy = jasmine.createSpyObj('LoadingController', ['create']);
+
+        const mockLoadingElement = {
+            present: jasmine.createSpy('present').and.returnValue(Promise.resolve()),
+            dismiss: jasmine.createSpy('dismiss').and.returnValue(Promise.resolve())
+        } as unknown as HTMLIonLoadingElement;
+        loadingControllerSpy.create.and.returnValue(Promise.resolve(mockLoadingElement));
+
+        await TestBed.configureTestingModule({
+            imports: [IonicModule.forRoot(), CharacterListComponent],
+            providers: [
+                { provide: ApiService, useValue: apiServiceSpy },
+                { provide: NotificationService, useValue: notificationServiceSpy },
+                { provide: LoadingController, useValue: loadingControllerSpy }
+            ]
         }).compileComponents();
 
         fixture = TestBed.createComponent(CharacterListComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
-    }));
+    });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe('Component Init', () => {
+        it('should display the list of characters after fetching from API', async () => {
+            apiServiceSpy.getCharacters.and.returnValue(of(mockMarvelApiResponse));
+    
+            fixture.detectChanges();
+            await fixture.whenStable();
+    
+            expect(apiServiceSpy.getCharacters).toHaveBeenCalledWith(0, 20);
+            expect(component.characters.length).toBe(2);
+            expect(component.characters[0].name).toBe('Spider-Man');
+            expect(component.characters[1].name).toBe('Iron Man');
+        });
+    
+        it('should show the error toast when no characters are found', async () => {
+            apiServiceSpy.getCharacters.and.returnValue(of(mockEmptyMarvelApiResponse));
+        
+            fixture.detectChanges();
+            await fixture.whenStable();
+        
+            expect(notificationServiceSpy.showErrorToast).toHaveBeenCalledWith('No characters found!');
+            expect(component.characters.length).toBe(0);
+        });
+    
+        it('should show the error toast on an API error', async () => {
+            const errorResponse = new Error('Internal Server Error');
+            apiServiceSpy.getCharacters.and.returnValue(throwError(() => errorResponse));
+            spyOn(console, 'error');
+    
+            fixture.detectChanges();
+            await fixture.whenStable();
+    
+            expect(notificationServiceSpy.showErrorToast).toHaveBeenCalledWith('Failed to load characters');
+            expect(console.error).toHaveBeenCalledWith('Error fetching characters:', errorResponse);
+        });
+    });
+
+    describe('openCharacterDetail', () => {
+        it('should present the loading controller with the correct values and dismiss it', async () => {
+            apiServiceSpy.getCharacterById.and.returnValue(of(mockMarvelApiResponse));
+            const mockModalElement = {
+                present: jasmine.createSpy('present').and.returnValue(Promise.resolve()),
+                dismiss: jasmine.createSpy('dismiss').and.returnValue(Promise.resolve())
+            };
+            component.characterDetailsModal = mockModalElement as any;
+
+            await component.openCharacterDetail(123);
+            await fixture.whenStable();
+            const loadingElement = await loadingControllerSpy.create.calls.mostRecent().returnValue;
+
+            expect(loadingControllerSpy.create).toHaveBeenCalledWith({
+                message: 'Loading character details...',
+                spinner: 'crescent'
+            });
+            expect(loadingElement.present).toHaveBeenCalled();
+            expect(mockModalElement.present).toHaveBeenCalled();
+            expect(loadingElement.dismiss).toHaveBeenCalled();
+        });
+
+        it('should present the character details modal', async () => {
+            apiServiceSpy.getCharacterById.and.returnValue(of(mockMarvelApiResponse));
+            const mockModalElement = {
+                present: jasmine.createSpy('present').and.returnValue(Promise.resolve()),
+                dismiss: jasmine.createSpy('dismiss').and.returnValue(Promise.resolve())
+            };
+            component.characterDetailsModal = mockModalElement as any;
+
+            await component.openCharacterDetail(123);
+            await fixture.whenStable();
+
+            expect(mockModalElement.present).toHaveBeenCalled();
+        });
+
+        it('should catch errors, display an error toast, and log the error to the console', async () => {
+            const mockModalElement = {
+                present: jasmine.createSpy('present').and.returnValue(Promise.reject('Modal present error')),
+                dismiss: jasmine.createSpy('dismiss').and.returnValue(Promise.resolve())
+            };
+            component.characterDetailsModal = mockModalElement as any;
+            spyOn(console, 'error');
+
+            await component.openCharacterDetail(123);
+            await fixture.whenStable();
+            const loadingElement = await loadingControllerSpy.create.calls.mostRecent().returnValue;
+
+            expect(mockModalElement.present).toHaveBeenCalled();
+            expect(notificationServiceSpy.showErrorToast).toHaveBeenCalledWith('Failed to open character details');
+            expect(console.error).toHaveBeenCalledWith('Error presenting character details modal:', 'Modal present error');
+            expect(loadingElement.dismiss).toHaveBeenCalled();
+        });
     });
 });
--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,7 +1,10 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { MarvelCharacter } from 'src/app/shared/models/marvel-character';
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import { IonicModule, IonModal, LoadingController } from "@ionic/angular";
+import { ApiService } from "src/app/core/services/api.service";
+import { NotificationService } from "src/app/core/services/notification.service";
+import { MarvelApiResponse, MarvelCharacter } from "src/app/shared/models/marvel-character";
+import { CharacterDetailComponent } from "../character-detail/character-detail.component";
 
 @Component({
     selector: 'app-character-list',
@@ -10,9 +13,62 @@ import { MarvelCharacter } from 'src/app/shared/models/marvel-character';
     standalone: true,
     imports: [
         IonicModule,
-        CommonModule
+        CommonModule,
+        CharacterDetailComponent
     ]
 })
-export class CharacterListComponent {
-    @Input() characters: MarvelCharacter[] = [];
+export class CharacterListComponent implements OnInit {
+    @Input() public characters: MarvelCharacter[] = [];
+    @ViewChild('characterDetailsModal') public characterDetailsModal?: IonModal;
+
+    public characterId = 0;
+    public offset = 0; // To keep track of pagination
+    public limit = 20; // Number of characters to fetch per API call
+    public loading = false;
+
+    constructor(
+        private loadingController: LoadingController,
+        private notificationService: NotificationService,
+        private apiService: ApiService
+    ) {}
+
+    public ngOnInit(): void {
+        this.loadInitialCharacters();
+    }
+    
+    private loadInitialCharacters(): void {
+        this.loading = true;
+        this.apiService.getCharacters(this.offset, this.limit).subscribe({
+            next: (response: MarvelApiResponse) => {
+                this.characters = response.data.results;
+                this.loading = false;
+                if (this.characters.length === 0) {
+                    this.notificationService.showErrorToast('No characters found!');
+                }
+            },
+            error: (error: any) => {
+                this.loading = false;
+                this.notificationService.showErrorToast('Failed to load characters');
+                console.error('Error fetching characters:', error);
+            }
+        });
+    }
+
+    public async openCharacterDetail(characterId: number): Promise<void> {
+        this.characterId = characterId;
+        const loadingCtrl = await this.loadingController.create({
+            message: 'Loading character details...',
+            spinner: 'crescent'
+        });
+        await loadingCtrl.present();
+    
+        try {
+            await this.characterDetailsModal?.present();
+        } catch (error) {
+            await this.notificationService.showErrorToast('Failed to open character details');
+            console.error('Error presenting character details modal:', error);
+        } finally {
+            await loadingCtrl.dismiss();
+        }
+    }
 }
