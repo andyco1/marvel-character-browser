cd944e1fc3f2dba6ecfc4b56a7a0b69e38a26a1e
Andrew Connor <andrewconnor2@gmail.com>
2024-10-06 20:09:00 +0100

    Updated environment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    marvelPublicKey: '8739547ecf75e73f9d4107cc3a008e74',
-    marvelPrivateKey: '1620bd774c765837aaa105e98944211f31873e74',
+    marvelPublicKey: 'YOUR_PUBLIC_KEY',
+    marvelPrivateKey: 'YOUR_PRIVATE_KEY',
     marvelApiUrl: 'https://gateway.marvel.com/v1/public'
 };
